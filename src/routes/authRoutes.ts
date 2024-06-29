3b4a8149498f04e0855d6962fc8ee23d88592941
Athulkrishna-S <athulkrishna768@gmail.com>
2024-06-29 21:54:50 +0530

    fix in build

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -29,7 +29,7 @@ interface User {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || "",
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: '/auth/google/callback'
 }, function verify(accessToken: string, refreshToken: string, profile: any, cb: VerifyCallback): void {
     // verification logic here
     console.log("Profile ",profile)
